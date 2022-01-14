313a145c030a9dfdad72e3db6b2f9efb87966f6e
Shamim Sikder <35293225+shamimsikder@users.noreply.github.com>
2022-01-14 13:19:37 +0600

    Fixing Some Error

--- ToDo List App/ToDoApps/settings.py
+++ ToDo List App/ToDoApps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7zbm+7xnx5u=f+h!vsr(2gs*dfa0^(hivfzf)#%b$!4=4wm*ka'
+SECRET_KEY = '' #inser your own secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
